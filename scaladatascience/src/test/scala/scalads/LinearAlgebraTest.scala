4f5ab9cfe9c70c8996479d5542ce228902038233
a-poor <a-poor@users.noreply.github.com>
2020-06-20 15:30:20 -0400

    Finishing adding tests for LinearAlgebra (1-each)

--- scaladatascience/src/test/scala/scalads/LinearAlgebraTest.scala
+++ scaladatascience/src/test/scala/scalads/LinearAlgebraTest.scala
@@ -1,7 +1,6 @@
 
 import org.scalatest._
 import flatspec._
-import matchers._
 
 import scalads._
 
@@ -14,15 +13,13 @@ class LinearAlgebraTest extends FunSuite {
             )
         assert(result == Seq(5,7,9))
     }
-
     test("LinearAlgebra.sub") {
         val result = LinearAlgebra.sub(
                 Seq(1,2,3),
                 Seq(4,5,6)
             )
-        assert(result == Seq(3.0,3.0,3.0))
+        assert(result == Seq(-3,-3,-3))
     }
-
     test("LinearAlgebra.vector_sum") {
         val result = LinearAlgebra.vector_sum(
             Seq(
@@ -33,7 +30,6 @@ class LinearAlgebraTest extends FunSuite {
         )
         assert(result == Seq(6,8,10))
     }
-
     test("LinearAlgebra.vector_mean") {
         val result = LinearAlgebra.vector_mean(
             Seq(
@@ -43,21 +39,47 @@ class LinearAlgebraTest extends FunSuite {
         )
         assert(result == Seq(2.5,3.5,4.5))
     }
-
     test("LinearAlgebra.scalar_multiply") {
         val result = LinearAlgebra.scalar_multiply(
             Seq(1,2,3),2
         )
         assert(result == Seq(2,4,6))
     }
-
     test("LinearAlgebra.dot") {
         val result = LinearAlgebra.dot(
             Seq(1,2,3),Seq(1,2,3)
         )
         assert(result == 14.0)
     }
-    
-
+    test("LinearAlgebra.sum_of_squares") {
+        val result = LinearAlgebra.sum_of_squares(
+            Seq(1,2,3)
+        )
+        assert(result == 14.0)
+    }
+    test("LinearAlgebra.magnitude") {
+        val result = LinearAlgebra.magnitude(
+            Seq(1,2,3)
+        )
+        assert(result == math.sqrt(14))
+    }
+    test("LinearAlgebra.squared_distance") {
+        val result = LinearAlgebra.squared_distance(
+            Seq(1,2,3),
+            Seq(4,5,6)
+        )
+        assert(result == 27)
+    }
+    test("LinearAlgebra.distance") {
+        val result = LinearAlgebra.distance(
+            Seq(1,2,3),
+            Seq(4,5,6)
+        )
+        assert(5.19 < result && result < 5.20)
+    }
+    test("LinearAlgebra.identity_matrix") {
+        val result = LinearAlgebra.identity_matrix(2)
+        assert(result == Seq(Seq(1,0),Seq(0,1)))
+    }
 }
 
